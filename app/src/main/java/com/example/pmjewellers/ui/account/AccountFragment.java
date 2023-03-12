4f248c4b29ab9a9e8fa26471e75f20df705358bb
om.p.shahane@gmail.com <om.p.shahane@gmail.com>
2023-03-12 12:39:51 +0530

    commit

--- app/src/main/java/com/example/pmjewellers/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/pmjewellers/ui/account/AccountFragment.java
@@ -1,7 +1,6 @@
 package com.example.pmjewellers.ui.account;
 
 import android.os.Bundle;
-<<<<<<< HEAD
 
 import androidx.fragment.app.Fragment;
 
@@ -11,14 +10,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.pmjewellers.MainActivity;
-=======
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
->>>>>>> 27eec2a (commit)
 import com.example.pmjewellers.R;
 
 /**
